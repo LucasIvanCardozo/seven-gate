85ecee06c7e0c2509df830ae4c5ac19b3e8fcd3f
Lucas Cardozo <lucasivancardozo27@gmail.com>
2025-06-21 20:06:32 -0300

    update styles

--- src/app/(pages)/conferences/[id]/components/LogoImage.tsx
+++ src/app/(pages)/conferences/[id]/components/LogoImage.tsx
@@ -3,23 +3,24 @@
 import { getConference } from "@/app/lib/actions/conferences/get.conference"
 import { Conference } from "@/app/lib/actions/conferences/get.my.conferences"
 import { redirect } from "next/navigation"
+import Styles from "./LogoImage.module.css"
+import Image from "next/image"
 
 export const LogoImage = async ({ id }: Pick<Conference, "id">) => {
     const { data } = await getConference({ id })
     if (!data) return redirect("/conferences")
 
     const { logo_url } = data
+
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                padding: "4rem 0 0 0",
-            }}
-        >
-            <img src={logo_url} style={{
-                borderRadius: 10
-            }} />
+        <div className={Styles.container}>
+            <Image
+                src="/images/redGtec.png"
+                height={150}
+                width={300}
+                alt=""
+                className={Styles.image}
+            />
         </div>
     )
 }

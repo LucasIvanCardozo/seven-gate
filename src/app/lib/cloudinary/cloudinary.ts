8d61e99338a323d47eb055d311d6f9009b621b89
Lucas Cardozo <lucasivancardozo27@gmail.com>
2025-06-23 21:01:24 -0300

    fix cloudinary

--- src/app/lib/cloudinary/cloudinary.ts
+++ src/app/lib/cloudinary/cloudinary.ts
@@ -16,6 +16,7 @@ cloudinary.config({
     cloud_name: CLOUDINARY_CLOUD_NAME,
     api_key: CLOUDINARY_API_KEY,
     api_secret: CLOUDINARY_API_SECRET,
+    secure: true,
 })
 
 export const uploadFile = async (file: File, options?: UploadApiOptions) => {
